4709f3c1a17fa2b524e9d8d50718471396ebd2ab
midhunarmid <midhunarmid@gmail.com>
2014-08-26 17:55:05 +0530

    First

---
+++ NoiseAlert/src/com/faisons/android/speedtest/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.faisons.android.speedtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the layout for this activity.  You can find it
+        // in res/layout/help.xml
+        
+    }
+}
